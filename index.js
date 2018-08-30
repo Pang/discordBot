b17813334bc6afb395bddc362a519ddd45360a34
cPango <ryan.beckett@outlook.com>
2018-08-30 14:29:22 +0100

    added bots first command

--- index.js
+++ index.js
@@ -4,12 +4,22 @@ const bot = new Discord.Client();
 
 bot.on('ready', async () => {
     console.log(`${bot.user.username} is online`);
-})
+});
 
-bot.on('message', msg => {
-    if (msg.content == "ping"){
-        msg.reply("Pong!");
+bot.on('message', async message => {
+    if(message.author.bot) return;
+    if(message.channel.type === "dm") return;
+
+    //The below allows for arguements to be seperated after the command.
+    let prefix = botconfig.prefix;
+    let messageArray = message.content.split(" ");
+    let cmd = messageArray[0];
+    let args = messageArray.slice(1);
+
+    if (cmd == `${prefix}hello`){
+        return message.channel.send("Hello!");
     }
-})
+
+});
 
 bot.login(botconfig.token);
