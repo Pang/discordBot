00be2c56bef72ac21e75af0371480f3d618f799a
cPango <ryan.beckett@outlook.com>
2018-08-30 13:16:06 +0100

    Got Bot online

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const Discord = require('discord.js');
 const botconfig = require("./botconfig.json");
 const bot = new Discord.Client();
 
-bot.on("ready", async () => {
+bot.on('ready', async () => {
     console.log(`${bot.user.username} is online`);
+})
+
+bot.on('message', msg => {
+    if (msg.content == "ping"){
+        msg.reply("Pong!");
+    }
 })
 
 bot.login(botconfig.token);
