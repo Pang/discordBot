ccf0be0904383bbddd06924681739b2b5c0d3f8a
cPango <ryan.beckett@outlook.com>
2018-08-30 16:53:32 +0100

    Added botinfo & serverinfo commands

--- index.js
+++ index.js
@@ -20,6 +20,30 @@ bot.on('message', async message => {
         return message.channel.send("Hello!");
     }
 
+    if (cmd == `${prefix}botinfo`){
+        let botEmbed = new Discord.RichEmbed()
+        .setDescription("Bot Information")
+        .setColor("2d998c")
+        .setThumbnail(bot.user.displayAvatarURL)
+        .addField("Bot Name", bot.user.username)
+        .addField("DoB", bot.user.createdAt);
+
+        return message.channel.send(botEmbed);
+    }
+
+    if (cmd === `${prefix}serverinfo`){
+        let serverEmbed = new Discord.RichEmbed()
+        .setDescription("Server Information")
+        .setColor("2d998c")
+        .setThumbnail(message.guild.displayAvatarURL)
+        .addField("Server Name", message.guild.name)
+        .addField("Created on", message.guild.createdAt)
+        .addField("You joined", message.member.joinedAt)
+        .addField("Total members", message.guild.memberCount);
+
+        return message.channel.send(serverEmbed);
+    }
+
 });
 
 bot.login(botconfig.token);
