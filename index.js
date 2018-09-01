3fb65c9a30135ede90d7bf14f520a52062c95c5d
cPango <ryan.beckett@outlook.com>
2018-09-01 19:29:31 +0100

    Added report command

--- index.js
+++ index.js
@@ -4,13 +4,14 @@ const bot = new Discord.Client();
 
 bot.on('ready', async () => {
     console.log(`${bot.user.username} is online`);
+    //bot.user.setActivity('being tested');
 });
 
 bot.on('message', async message => {
     if(message.author.bot) return;
     if(message.channel.type === "dm") return;
 
-    //The below allows for arguements to be seperated after the command.
+    //Allows for arguements to be seperated after the command.
     let prefix = botconfig.prefix;
     let messageArray = message.content.split(" ");
     let cmd = messageArray[0];
@@ -42,6 +43,29 @@ bot.on('message', async message => {
         .addField("Total members", message.guild.memberCount);
 
         return message.channel.send(serverEmbed);
+    }
+
+    if (cmd === `${prefix}report`){
+
+        let reportedUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+        if(!reportedUser) return message.channel.send("Couldn't find user.");
+
+        //removes the first arguement being the username and joins the rest together
+        args.shift();
+        let reason = args.join(" ");
+
+        let reportEmbed = new Discord.RichEmbed()
+        .setDescription("Reports")
+        .setColor("2d998c")
+        .addField("Reported User", `${reportedUser} with ID: ${reportedUser.id}`)
+        .addField("Reported by", `${message.author} with ID: ${message.author.id}`)
+        .addField("Time", message.createdAt)
+        .addField("Reason", reason);
+
+        let reportsChannel = message.guild.channels.find(x => x.name === "reports-and-tests");
+        if(!reportsChannel) return message.channel.send("Couldn't find report/test channel.");
+
+        return reportsChannel.send(reportEmbed);
     }
 
 });
